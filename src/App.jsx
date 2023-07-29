a1034827fe9db6825332e80580832e181afb9714
zednegr <negrzed@gmail.com>
2023-07-29 14:27:26 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Header from './components/header/header'
 
 function App() {
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,72 @@
+import "./header.scss"
+import {useState, useEffect } from "react"
+import MovieStar from "../../assets/svg/movie-star.svg"
+import MovieClock from "../../assets/svg/movie-clock.svg"
+
+function Header() {
+
+    const [getData, setgetData] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('https://64b8278821b9aa6eb0799f1f.mockapi.io/movie/films')
+            .then((res) => res.json())
+            .then((data) => setgetData(data))
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [])
+
+    if (loading) {
+        return <h1>loading</h1>
+    }
+
+    return (
+
+        <div className="section">
+            <div className="container">
+                <div className="movie-about">
+                    <div className="about"><h1>Upcoming Movies</h1></div>
+                </div>
+
+                <div className="wrapper">
+
+                    {
+                        getData?.map((item, index) => {
+                            return (
+                                <div className="movie-cards-wrapper">
+                                    <div className="card-movie">
+                                        <div className="card-avatar">
+                                            <img src={item.img} alt="Movie" />
+                                        </div>
+                                        <div className="card-desc">
+
+                                            <div className="movie-name"><h4 className='movie-name-h4'>{item.name.slice(0, 12)}</h4></div>
+                                            <div className="movie-year"><h5 className='movie-year-h5'>{item.year}</h5></div>
+
+                                        </div>
+                                        <div className="card-data">
+
+                                            <div className="card-data-qualty"><p className='card-data-qualty-p'>{item.quality}</p></div>
+
+                                            <div className="card-min-reyting">
+
+                                                <div className="card-min"><img className='MovieClock' src={MovieClock} width={'13px'} /><p className='card-min-p'>130 min</p></div>
+                                                <div className="card-reyting"><img className='MovieStar' src={MovieStar} width={'12px'} /><p className='card-reyting-p'>7.0</p></div>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
