28173c94c1ffa69b17445660617eeaa3e537857c
zednegr <negrzed@gmail.com>
2023-08-03 01:54:09 +0500

    update

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -10,23 +10,25 @@ function Hero() {
 
     return (
         <section className='hero-section'>
-                    <Navbar />
 
             <Swiper
+
+                slidesPerView={1}
                 spaceBetween={30}
+                loop={true}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 2500,
+                    delay: 1500,
                     disableOnInteraction: false,
                 }}
                 pagination={{
                     clickable: true,
                 }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
+                modules={[Autoplay, Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide className='SliederOne'>
+                <SwiperSlide className='SliderOne'>
+                    <Navbar />
 
                     <div className="hero-wrapper">
                         <div className="container">
@@ -69,7 +71,48 @@ function Hero() {
 
                 </SwiperSlide>
 
-                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide className='SliderTwo'>
+                    <Navbar />
+
+                    <div className="hero-wrapper">
+                        <div className="container">
+
+                            <div className="hero-about">
+                                <div className="hero-brand">
+                                    <h4 className='hero-brand-h4'>Filmagnet</h4>
+                                </div>
+
+                                <div className="hero-title">
+                                    <h1 className='hero-title-h1'>Filmagnet Unlimited Entertainment, Movies, TVs shows, & More.</h1>
+                                </div>
+
+                                <div className="hero-info">
+                                    <div className="hero-info-dramma">
+                                        <h5 className='hero-info-dramma-h5'>Movie</h5>
+                                    </div>
+
+                                    <div className="hero-info-quality">
+                                        <h5 className='hero-info-quality-h5'>FHD</h5>
+                                    </div>
+
+                                    <div className="hero-info-type">
+                                        <h5 className='hero-info-type-h5'>Action, Drama</h5>
+                                    </div>
+
+                                    <div className="hero-info-calendar">
+                                        <h5 className='hero-info-calendar-h5'>2023</h5>
+                                    </div>
+                                </div>
+
+                                <div className="hero-play">
+                                    <div className="hero-play-wrapper">
+                                        <button className='hero-play-btn'>Play Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
             </Swiper>
 
 
