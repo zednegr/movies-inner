9d21ca7a61954bb852c9114298c48c140fe35fb7
zednegr <negrzed@gmail.com>
2023-08-03 00:28:03 +0500

    update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,16 +8,26 @@ function Navbar() {
 
         <section className='navbar-section'>
             <div className="container">
-                <div className="brand">
-                    <img src={MoviesLogo} alt="Logo" />
-                </div>
                 <nav className='navbar'>
+
+                    <div className="brand">
+                        <img src={MoviesLogo} alt="Logo" />
+                        <h2 className='brand-h2'>Filmagnet</h2>
+                    </div>
+
                     <ul className='nav_list'>
                         <li><a href="">Home</a></li>
+                        <span>|</span>
                         <li><a href="">Home</a></li>
+                        <span>|</span>
                         <li><a href="">Home</a></li>
+                        <span>|</span>
                         <li><a href="">Home</a></li>
                     </ul>
+
+                    <div className="nav_right">
+                        <a className='nav-btn' href="#">Sign in</a>
+                    </div>
                 </nav>
             </div>
 
