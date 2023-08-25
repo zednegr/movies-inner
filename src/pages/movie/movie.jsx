8aa06070ed4ed062aa47eb22653139d25622022a
zednegr <negrzed@gmail.com>
2023-08-25 22:57:43 +0500

    update

--- src/pages/movie/movie.jsx
+++ src/pages/movie/movie.jsx
@@ -18,8 +18,8 @@ function Movie() {
     console.log(movieId);
 
     useEffect(() => {
-        axios('https://64ca9c72700d50e3c7051e26.mockapi.io/movie/movies')
-        .then(res => console.log(res))
+        axios('https://64e8e93299cf45b15fe0502c.mockapi.io/movie/movie')
+            .then(res => console.log(res))
             .finally(() => {
                 setLoading(false)
             })
